a80b97a8c0599ff32ed6d5c18719b371a820eed6
rcaldwell1200 <robert.caldwell@elitepartyof6.com>
2017-09-05 14:39:20 -0500

    Added netstat component
    Added netstat collection scripting

--- Collector.ps1
+++ Collector.ps1
@@ -6,4 +6,19 @@ $usage = 100-$pctFree
 $free = Get-WmiObject win32_logicaldisk -Filter "DeviceID='C:'" | select -ExpandProperty FreeSpace
 $size = Get-WmiObject win32_logicaldisk -Filter "DeviceID='C:'" | select -ExpandProperty Size
 $dskstat = [math]::Round(($free/$size)*100,2)
-Invoke-Sqlcmd -Database Scaling -ServerInstance "EP6SQL01\REPORTING" -Query "INSERT INTO hoststat VALUES ('$now','$env:COMPUTERNAME','1','$usage','$cpustat','$dskstat')"
+#network
+$colInterfaces = Get-WmiObject -class Win32_PerfFormattedData_Tcpip_NetworkInterface |select BytesTotalPersec, CurrentBandwidth,PacketsPersec|where {$_.PacketsPersec -gt 0}
+
+foreach ($interface in $colInterfaces) {
+$bitsPerSec = $interface.BytesTotalPersec * 8
+$totalBits = $interface.CurrentBandwidth
+
+# Exclude Nulls (any WMI failures)
+if ($totalBits -gt 0) {
+$result = (( $bitsPerSec / $totalBits) * 100)
+}
+}
+$netstat = $result*100
+ 
+
+Invoke-Sqlcmd -Database Scaling -ServerInstance "EP6SQL01\REPORTING" -Query "INSERT INTO hoststat VALUES ('$now','$env:COMPUTERNAME','$netstat','$usage','$cpustat','$dskstat')"
